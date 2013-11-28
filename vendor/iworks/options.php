09ec3b45658c89d5994dfb16a82f117d6a784a1c
Marcin Pietrzak <marcin@iworks.pl>
2013-11-28 12:22:37 +0100

    clearing

--- vendor/iworks/options.php
+++ vendor/iworks/options.php
@@ -45,7 +45,7 @@ class IworksOptions
     public function __construct()
     {
         $this->notices              = array();
-        $this->version              = '1.7.4';
+        $this->version              = 'trunk';
         $this->option_group         = 'index';
         $this->option_function_name = null;
         $this->option_prefix        = null;
@@ -691,26 +691,3 @@ class IworksOptions
         return $this->option_group;
     }
 }
-
-/**
-
-== Changelog ==
-
-= 1.7.4 (2013-08-27) =
-
-* #IMPROVMENT: added filter to change options
-* #IMPROVMENT: added get_option_group function
-* #IMPROVMENT: added helper for wp_dropdown_categories
-* #IMPROVMENT: added helper for wp_dropdown_pages
-
-= 1.7.3 (2013-06-04) =
-
-* #BUGFIX: repair get_option, to prevent return always default if null
-* #IMPROVMENT: added force_default to get_option method
-
-= 1.7.2 (2013-05-23) =
-
-* IMPROVMENT: add min/max attributes to filed type "number"
-* #IMPROVMENT: add get_option_name method
-
- */
