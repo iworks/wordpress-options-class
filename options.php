7c42a78a25650838f856d70ed8d75e410b3f5d79
Marcin Pietrzak <marcin@iworks.pl>
2017-05-09 20:40:52 +0200

    add hex2rgb converter

--- options.php
+++ options.php
@@ -1226,4 +1226,27 @@ function iworks_options_tabulator_init()
 <?php
     }
 
+    /**
+     * Cnvert color to rgb
+     *
+     * @since 2.4.1
+     * 
+     * @param string $hex Hex value of color
+     * @return array RGB array.
+     */
+	public function hex2rgb($hex) {
+		$hex = str_replace("#", "", $hex);
+	
+		if(strlen($hex) == 3) {
+			$r = hexdec(substr($hex,0,1).substr($hex,0,1));
+			$g = hexdec(substr($hex,1,1).substr($hex,1,1));
+			$b = hexdec(substr($hex,2,1).substr($hex,2,1));
+		} else {
+			$r = hexdec(substr($hex,0,2));
+			$g = hexdec(substr($hex,2,2));
+			$b = hexdec(substr($hex,4,2));
+		}
+		$rgb = array($r, $g, $b);
+		return $rgb; // returns an array with the rgb values
+	}
 }
