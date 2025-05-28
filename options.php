f215c9e38791ead18d90fc648bf7ee52f9649d68
Marcin Pietrzak <marcin@iworks.pl>
2025-05-28 09:57:19 +0200

    change version

--- options.php
+++ options.php
@@ -3,7 +3,7 @@
 Class Name: iWorks Options
 Class URI: http://iworks.pl/
 Description: Option class to manage options.
-Version: 3.0.6
+Version: 3.0.7
 Author: Marcin Pietrzak
 Author URI: http://iworks.pl/
 License: GPLv3 or later
@@ -74,7 +74,7 @@ class iworks_options {
 		 * basic setup
 		 */
 		$this->notices              = array();
-		$this->version              = '3.0.6';
+		$this->version              = '3.0.7';
 		$this->option_group         = 'index';
 		$this->option_function_name = null;
 		$this->option_prefix        = null;
@@ -2698,6 +2698,7 @@ class iworks_options {
 				'class'             => true,
 				'data-*'            => true,
 				'id'                => true,
+				'height'            => true,
 				'name'              => true,
 				'sandbox'           => true,
 				'scrolling'         => true,
